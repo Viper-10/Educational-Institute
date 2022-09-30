46c39fbf6ad0ef7c002f5a2de4b91de5b2109a36
Viper-10 <priyadharshanbeepee@gmail.com>
2022-09-30 15:33:30 +0530

    Add - complete about page

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
 "use strict";
 let slideIndex = 1;
+let currentPage = "home";
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -30,7 +31,8 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-const mobileSideBarMenu = document.querySelector("#mobile-navbar");
+const mobileSideBarMenu = document.getElementById("mobile-navbar");
+const mobileSideBarMenu2 = document.getElementById("mobile-navbar-2");
 let isDropdownOpen = false;
 
 window.addEventListener("click", handleClick);
@@ -38,17 +40,54 @@ window.addEventListener("click", handleClick);
 function handleClick(e) {
   if (e.target.classList.contains("fa-bars")) {
     isDropdownOpen = !isDropdownOpen;
-    changeDropdownVisibility();
+    changeDropdownVisibility(
+      currentPage === "home" ? mobileSideBarMenu2 : mobileSideBarMenu
+    );
   } else if (!mobileSideBarMenu.contains(e.target)) {
     isDropdownOpen = false;
-    changeDropdownVisibility();
+    changeDropdownVisibility(
+      currentPage === "home" ? mobileSideBarMenu2 : mobileSideBarMenu
+    );
   }
 }
 
-function changeDropdownVisibility() {
+function changeDropdownVisibility(mobileSideBarMenu) {
   if (isDropdownOpen) {
     mobileSideBarMenu.style.display = "block";
   } else {
     mobileSideBarMenu.style.display = "none";
   }
 }
+
+const aboutPageButton = document.querySelector("#about-page-button");
+const aboutPage = document.querySelector("#about-page");
+const homePage = document.querySelector("#home-page");
+const homePageButton = document.querySelector("#home-page-button");
+aboutPageButton.addEventListener("click", showAboutPage);
+homePageButton.addEventListener("click", showHomePage);
+
+function showAboutPage() {
+  currentPage = "about";
+  window.scrollTo(0, 0);
+
+  aboutPage.style.display = "block";
+  aboutPageButton.style.display = "none";
+  homePage.style.display = "none";
+
+  homePageButton.style.display = "block";
+}
+
+function showHomePage() {
+  currentPage = "home";
+  window.scrollTo(0, 0);
+  homePageButton.style.display = "none";
+  homePage.style.display = "block";
+  aboutPageButton.style.display = "block";
+  aboutPage.style.display = "none";
+}
+
+const logoDivs = document.getElementsByClassName("logo-wrapper");
+
+Array.from(logoDivs).forEach((element) => {
+  element.addEventListener("click", showHomePage);
+});
